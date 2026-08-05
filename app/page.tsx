71ada26ec7595876570ccbe71eb95287af1cc131
Rama Armytha <aderamadhaniputra35@gmail.com>
2026-08-05 21:46:07 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Portfolio from '@/components/Portfolio'
-
-export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-[#0f172a] to-[#1e293b]">
-      <Portfolio />
-    </main>
-  )
-}
